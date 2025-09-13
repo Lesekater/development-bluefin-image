6dbbf36e3d0d89529b94f2c7fdb2f8f2c8e91bdc
Elias Wernicke <e.w.1111111119@gmail.com>
2025-09-13 17:17:36 +0200

    cleanup extra packages

--- build_files/install-apps.sh
+++ build_files/install-apps.sh
@@ -17,18 +17,14 @@ declare -A RPM_PACKAGES=(
     nmap \
     thefuck \
     fish \
-    rawtherapee \
     spotify-client \
     deja-dup \
-    Signal-Desktop"
+    blender.x86_64 \
+    bottles.noarch \
+    gimp.x86_64"
 
   ["terra"]="\
     starship"
-
-#   ["rpmfusion-free,rpmfusion-free-updates,rpmfusion-nonfree,rpmfusion-nonfree-updates"]="\
-#     audacious \
-#     audacious-plugins-freeworld \
-#     audacity-freeworld"
 
   ["fedora-multimedia"]="\
     HandBrake-cli \
@@ -60,34 +56,6 @@ for repo in "${!RPM_PACKAGES[@]}"; do
     cmd+=("${pkg_array[@]}")
     "${cmd[@]}"
   fi
-done
-
-# flatpak packages list
-declare -A FLATPAK_PACKAGES=(
-  ["flathub"]="\
-    com.usebottles.bottles \
-    org.blender.Blender \
-    org.gimp.GIMP \
-    org.libreoffice.LibreOffice \
-    org.signal.Signal \
-    org.telegram.desktop \
-    com.discordapp.Discord"
-)
-
-# Install Flatpaks
-log "Installing Flatpaks"
-for repo in "${!FLATPAK_PACKAGES[@]}"; do
-  for flatpak_id in ${FLATPAK_PACKAGES[$repo]}; do
-    [[ -z "$flatpak_id" ]] && continue
-    if ! flatpak remote-list | grep -q "^$repo\s"; then
-      flatpak remote-add --if-not-exists "$repo" "https://flathub.org/repo/$repo.flatpakrepo"
-    fi
-    if ! flatpak list --app | grep -q "^$flatpak_id\s"; then
-      flatpak install -y --noninteractive --system "$repo" "$flatpak_id"
-    else
-      log "$flatpak_id is already installed, skipping."
-    fi
-  done
 done
 
 log "Build process completed"
